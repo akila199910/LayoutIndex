ca563dbe8e5ca0f7a6ad21edc8beccedfb492d0f
Akila-PHP <143219353+akila199910@users.noreply.github.com>
2025-03-25 22:54:39 +0530

    imstall packages and add helper file

--- rms/database/migrations/2014_10_12_000000_create_users_table.php
+++ rms/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('ref_no')->nullable();
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email')->index('email');
+            $table->string('contact')->index('contact');
+            $table->integer('status')->default(1);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
